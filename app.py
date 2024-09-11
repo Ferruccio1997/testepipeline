095c35c871f8de89ef30dcae93248ea4a9c6ffb4
Ferruccio Ribeiro <ferruccioribeiro@outlook.com>
2024-09-10 23:40:06 -0300

    first 2 commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+from contrato import Vendas
 from datetime import datetime, time
+from pydantic import ValidationError
 
 def main():
     st.title("Sistema de CRM e Vendas da ZapFlow - Frontend Simples")
@@ -12,15 +14,21 @@ def main():
 
 
     if st.button("Salvar"):
+        
+        try:
+            data_hora = datetime.combine(data, hora)
 
-        data_hora = datetime.combine(data, hora)
-        st.write("**Dados de venda:**")
-        st.write(f"Email do vendedor: {email}")
-        st.write(f"Data e Hora da Compra: {data_hora}")
-        st.write(f"Valor da Venda: R$ {valor:.2f}")
-        st.write(f"Quantidade de Produtos: {quantidade}")
-        st.write(f"Produto: {produto}")
-
+            venda = Vendas(
+                email = email,
+                data = data_hora,
+                valor = valor,
+                quantidade = quantidade,
+                produto = produto
+            )
+            st.write(venda)
+            
+        except ValidationError as e:
+            st.error(f"Deu erro {e}")
 
 if __name__=="__main__":
     main()
